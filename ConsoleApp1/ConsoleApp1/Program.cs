2ffa738d06fe26add03c01bc650e9ad2a16736d3
Your Artem <arte20012016@mail.ru>
2019-02-26 18:31:42 +0300

    commit1.2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,10 +6,46 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    public class student
+    {
+
+        public string name { get; set; }
+        public int course { get; set; }
+        public int floor { get; set; }
+
+        public student()
+        {
+            name = "";
+            course = 0;
+            floor = 0;
+        }
+
+        public void vvod()
+        {
+            Console.WriteLine("Добавление нового студента => ");
+            Console.Write("Имя: ");
+            name = Console.ReadLine();
+            Console.Write("Курс: ");
+            course = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Пол: ");
+            floor = Convert.ToInt16(Console.ReadLine());
+        }
+
+        public void vivod()
+        {
+            Console.WriteLine("Имя: {0}, Курч: {1}, Пол {2}", name, course, floor);
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            student student = new student();
+            student.vvod();
+            student.vivod();
+            Console.ReadKey();
         }
     }
 }
